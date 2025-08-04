62a018e5d6b5a6c1204749985d9a71c34d41b8d9
VishalSaini7272 <sainivishal156@gmail.com>
2025-08-04 10:59:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function firstWord(s) {
   // your code here
-	 str = str.trimStart();
+	 s = s.trimStart();
 
   // Find the index of the first space
-  const firstSpaceIndex = str.indexOf(' ');
+  const firstSpaceIndex = s.indexOf(' ');
 
   // If there's no space, return the entire string
   if (firstSpaceIndex === -1) {
-    return str;
+    return s;
   }
-return str.slice(0, firstSpaceIndex);
+return s.slice(0, firstSpaceIndex);
 }
 
 
