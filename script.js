36d90521477dcec052743ab9443599b5f04239dc
VishalSaini7272 <sainivishal156@gmail.com>
2025-08-04 10:56:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function firstWord(s) {
   // your code here
+	 str = str.trimStart();
+
+  // Find the index of the first space
+  const firstSpaceIndex = str.indexOf(' ');
+
+  // If there's no space, return the entire string
+  if (firstSpaceIndex === -1) {
+    return str;
+  }
+return str.slice(0, firstSpaceIndex);
 }
+
 
 // Do not change the code below
 
